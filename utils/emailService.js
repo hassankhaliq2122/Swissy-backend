63bffca3d936e35d4f4e51ba42769003b9665350
Codes <hassankhaliq2122@gmail.com>
2025-12-11 16:13:45 +0500

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -19,9 +19,7 @@ const sendEmail = async ({ email, subject, html, attachments }) => {
     }
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      service: "gmail",
       auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
     });
 
