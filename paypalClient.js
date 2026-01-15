a2137463e21ee31f75d6c5f21dd108924262c030
Codes <hassankhaliq2122@gmail.com>
2026-01-15 23:39:45 +0500

    Update paypalClient.js

--- paypalClient.js
+++ paypalClient.js
@@ -1,8 +1,8 @@
 // backend/paypalClient.js
 const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
 
-const clientId = process.env.PAYPAL_CLIENT_ID || 'AYfcaXSqzdjrE6iXssROI4RcxC6IfTWuOKGXL_afsJ6dPH-VryHkUOkls3GuX8ecauGTT7gnceaTi4lJ';
-const clientSecret = process.env.PAYPAL_SECRET || 'EHaCr4tjaLNZemQhRmrmh4zYW3YkyOBK9IaRXG33lqx8qE5UB3epuJxXSkSEk6tQLyzgBjtcbelqqbmI';
+const clientId = process.env.PAYPAL_CLIENT_ID  ;
+const clientSecret = process.env.PAYPAL_SECRET ;
 
 const environment = process.env.NODE_ENV === 'production'
   ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
